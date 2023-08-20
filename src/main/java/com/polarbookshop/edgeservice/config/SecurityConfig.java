02e6ed6384ad49159102b33b9a186985f884521e
Hans Hellgren <hans.hellgren@cag.se>
2023-08-20 15:29:10 +0200

    Chapter 11.4

--- src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -2,16 +2,28 @@ package com.polarbookshop.edgeservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
   @Bean
-  SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity httpSecurity) {
+  SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity httpSecurity, ReactiveClientRegistrationRepository clientRegistrationRepos) {
     return httpSecurity
         .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
-        .formLogin(Customizer.withDefaults())
+        .oauth2Login(Customizer.withDefaults())
+//        .formLogin(Customizer.withDefaults())
+        .logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepos)))
         .build();
+  }
+  private ServerLogoutSuccessHandler oidcLogoutSuccessHandler(ReactiveClientRegistrationRepository clientRegistrationRepos) {
+    var oidcLogoutSuccessHandler = new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepos);
+    oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
+    return oidcLogoutSuccessHandler;
   }
 }
