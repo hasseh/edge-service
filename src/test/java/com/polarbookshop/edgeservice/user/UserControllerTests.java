0873a33343707d057c661929e4feaa3fd8a6735c
Hans Hellgren <hans.hellgren@cag.se>
2023-09-15 16:12:40 +0200

    Chapter 11.6

--- src/test/java/com/polarbookshop/edgeservice/user/UserControllerTests.java
+++ src/test/java/com/polarbookshop/edgeservice/user/UserControllerTests.java
@@ -8,13 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
 import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @WebFluxTest(UserController.class)
 @Import(SecurityConfig.class)
@@ -44,6 +48,27 @@ public class UserControllerTests {
         .expectStatus().is2xxSuccessful()
         .expectBody(User.class)
         .value(user -> assertThat(user).isEqualTo(expectedUser));
+  }
+
+  @Test
+  void whenLogoutAuthenticatedAndWithCsrfTokenThen302() {
+    when(reactiveClientRegistrationRepository.findByRegistrationId("test")).thenReturn(Mono.just(testClientRegistration()));
+    webClient.mutateWith(SecurityMockServerConfigurers.mockOidcLogin())
+        .mutateWith(SecurityMockServerConfigurers.csrf())
+        .post()
+        .uri("/logout")
+        .exchange()
+        .expectStatus().isFound();
+  }
+
+  private ClientRegistration testClientRegistration() {
+    return ClientRegistration.withRegistrationId("test")
+        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+        .clientId("test")
+        .authorizationUri("https://sso.polarbookshop.com/auth")
+        .tokenUri("https://sso.polarbookshop.com/token")
+        .redirectUri("https://polarbookshop.com")
+        .build();
   }
 
   private SecurityMockServerConfigurers.OidcLoginMutator configureMockOidcLogin(User expectedUser) {
